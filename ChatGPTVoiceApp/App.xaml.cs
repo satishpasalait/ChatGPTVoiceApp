bf0788e69c7366ee2e80353515f3fbac906b3503
Satish Pasala <spasala@logixhealth.com>
2024-04-10 16:37:48 -0400

    Creating a MAUI base solution.

---
+++ ChatGPTVoiceApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatGPTVoiceApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ChatGPTVoiceApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatGPTVoiceApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
